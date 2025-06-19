5c0ebd4066f66ee94cb845b9158a15320f80c77a
Mirian Quispe <mirianquispe1505@gmail.com>
2025-06-19 16:42:56 -0300

    feat: expose styles in module federation configuration

--- home/module-federation.config.ts
+++ home/module-federation.config.ts
@@ -2,6 +2,7 @@ export const mfConfig = {
   name: "home",
   shared: ["react", "react-dom"],
   exposes: {
+    "./styles": "./src/index.css",
     "./Header": "./src/components/header.tsx",
     "./Footer": "./src/components/footer.tsx",
   },
